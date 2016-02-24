6a48aabffb04c45ae1c7af174b00b376a04d57ba
Web Superhero <superhero@itfactory.neta>
2016-02-24 17:54:09 +0100

    Create new files

---
+++ src/js/main.js
@@ -0,0 +1,12 @@
+// Userek lekérése.
+jQuery.getJSON( 'users', function( users ) {
+    console.log( 'users', users );
+    
+    var unDef = 1;
+    
+    if ( true ) {
+        console.log(unDef);
+    }
+});
+
+// 
